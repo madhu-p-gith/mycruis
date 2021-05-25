f9122bff8aae2984610639e38fe4a35e6d79d1f2
madhu <sai.vikasvarma.p@gmail.com>
2021-05-25 11:53:44 +0530

    MCT5-102Display

---
+++ src/component/Display.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Display extends React.Component {
+constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+}
+
+    render() {
+        return <div>hello</div>;
+    }
+}
+
+
+
+export default Display;
